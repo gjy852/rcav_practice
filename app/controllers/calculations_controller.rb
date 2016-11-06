ab4609c50993bc283baf8588a4b92308d0dd0c4d
Grace Yeh <gyeh@users.noreply.github.com>
2016-11-06 14:30:54 -0600

    Grace's rcav_practice code (part 1)

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,20 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @id = params[:id]
+    @square = @id.to_i ** 2
+  end
+
+  def square_root
+    @id = params[:id]
+    @square_root = Math.sqrt (@id.to_i)
+  end
+
+  def random
+  end
+
+  def payment
+  end
 end
